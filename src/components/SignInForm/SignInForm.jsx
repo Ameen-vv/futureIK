9e813cc4cd3b9ec281fde10e1e3ff29d13a02b2c
Ameen-vv <mohammedameenvv@gmail.com>
2023-06-01 12:50:22 +0530

    final

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -35,7 +35,7 @@ const SignInForm = () => {
                     <div className="mb-4">
                         <label htmlFor="username" className="block font-medium mb-2">Email</label>
                         <input
-                            type="text"
+                            type="email"
                             id="username"
                             className="w-full border-gray-300 border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
                             placeholder="Enter your username"
